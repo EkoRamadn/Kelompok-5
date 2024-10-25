4012cde36e2eda9520ef419157b1cc6f545377bb
EkoRamadn <ekor4m4d4n1@gmail.com>
2024-10-25 20:49:21 +0700

    upload tugas

--- tugasKelompok.cpp
+++ tugasKelompok.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     string kata, kataBalik;
 
-    cout << "Chek Kata" << "\n";
+    cout << "Check Kata" << "\n";
     cout << "Input Kata : ";
     cin >> kata;
 
