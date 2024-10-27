bc26921e03f0b1a44bc518e1eb91a341fd059018
EkoRamadn <ekor4m4d4n1@gmail.com>
2024-10-27 22:31:18 +0700

    Revisi Tugas Kelompok 4

--- tugasKelompok.cpp
+++ tugasKelompok.cpp
@@ -2,22 +2,18 @@
 #include <algorithm>
 using namespace std;
 
-
 int main(){
     string kata, kataBalik;
 
     cout << "Check Kata" << "\n";
     cout << "Input Kata : ";
     cin >> kata;
-    
-    // copy isi kata
+
     kataBalik = kata;
 
-    //mengunakan fungsi reverse()
     reverse(begin(kataBalik), end(kataBalik));
 
-    string hasil = (kata.compare(kataBalik) == 0) ? "True" : "false";
-    cout << "hasil : " << hasil;
+    cout << "hasil : " << ((kata.compare(kataBalik) == 0) ? "True" : "false");
 
     return 0;
 }
